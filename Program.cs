b957ebc57bb1fd627588206eaf997658ef15d3a1
PeachCube <nathanpeach500@live.com>
2023-11-15 14:30:55 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPGPrototype_NathanPeach
+{
+    internal class Program
+    {
+        //Global variables
+        static int health;//(player health only)
+        static bool gameOver;
+        //Global variables
+        
+        static char[,] map = new char[,] 
+        {
+            { }, 
+            { }, 
+            { }, 
+            { }, 
+            { }, 
+            { },
+        };
+        static int width = map.GetLength(1);
+        static int height = map.GetLength(0);
+        
+        static void Main(string[] args)
+        {
+            
+            //Game loop
+            while (gameOver == false)
+            {
+                PlayerControl();
+            }
+
+        
+        
+        
+        
+        
+        
+        }
+        static void DisplayMap()
+        {
+
+        }
+        static void PlayerControl()
+        {
+
+        }
+    }
+}
