da4a91b70b23f818387d2243ac867e49457bbf78
PeachCube <nathanpeach500@live.com>
2023-11-20 09:59:46 -0400

    The "Map.txt" file is now displayed as an array using a nested for loop to allow for an in-game coordinate system

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.ComponentModel;
+using System.Threading;
 
 namespace TextRPGPrototype_NathanPeach
 {
@@ -12,10 +14,14 @@ namespace TextRPGPrototype_NathanPeach
         //Global variables
         static int health;//(player health only)
         static bool gameOver;
+        static string map = @"Map.txt";
+        static string[] display = File.ReadAllLines(map);
+        
         //Global variables
         
         static void Main(string[] args)
         {
+            Console.SetWindowSize(130,30);
             DisplayMap();
             
             //Game loop
@@ -33,9 +39,18 @@ namespace TextRPGPrototype_NathanPeach
         }
         static void DisplayMap()
         {
-            string map = @"Map.txt";
-            string display = File.ReadAllText(map);
-            Console.Write(display);
+            for (int y = 0; y < display.Length; y++)
+            {
+                string row = display[y];
+                for (int x = 0; x < row.Length; x++)
+                {
+                    char tile = row[x];
+                    Console.Write(tile);
+                    Thread.Sleep(1);
+                }
+                Thread.Sleep(1);
+                Console.WriteLine();
+            }
         }
         static void PlayerControl()
         {
