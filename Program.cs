db72aed8e4da5e29270714461f5a02ea858f0388
PeachCube <nathanpeach500@live.com>
2023-11-28 15:59:42 -0400

    created a map array that reads the display array to make a 2d array

--- Program.cs
+++ Program.cs
@@ -14,37 +14,45 @@ namespace TextRPGPrototype_NathanPeach
     {
         //Global variables
         static int health;//(player health only)
-        static bool gameOver; 
-        static string[] display = File.ReadAllLines(@"Map.txt");
-        static string[,] mapx = new string[,] { {  },{  } };
+        static bool gameOver;
+        static string[] display;
+        static char[,] map;
+        static int width;
+        static int height;
         //Global variables
-        
+
         static void Main(string[] args)
         {
-            Console.SetWindowSize(130,30);
+            display = File.ReadAllLines(@"Map.txt");//declares display equal to the strings on Map.txt (fills the display array with the string array on Map.txt)
+            height = display.Length;//the length of the display array from line 0 to the last line (the height of the array)
+            width = display[0].Length;//the length of a single line in the display array (the width of the line)
+            map = new char[width,height];//a 2d array with the same dimensions as display
+            
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    map[x,y] = display[y][x];//fills the 2d map array with the data in the display array by iterating through the dimensions of the display array with for loops
+                }
+            }
+
+            Console.SetWindowSize(width+1,height+1);
             DisplayMap();
-            Console.SetCursorPosition(10,10);
+            Console.SetCursorPosition(10,10);//player starting position
+            
             //Game loop
             while (gameOver == false)
             {
                 PlayerControl();
             }
-        
-        
-        
-        
-        
-        
         }
         static void DisplayMap()
         {
-            for (int y = 0; y < display.Length; y++)
+            for (int y = 0; y < height; y++)
             {
-                string row = display[y];
-                for (int x = 0; x < row.Length; x++)
+                for (int x = 0; x < width; x++)
                 {
-                    Console.Write(row[x]);
-                    //Thread.Sleep(1);
+                    Console.Write(map[x,y]);//uses a nested for loop to display each coordinate in the map array
                 }
                 Console.WriteLine();
             }
@@ -68,7 +76,6 @@ namespace TextRPGPrototype_NathanPeach
             {
                 Console.SetCursorPosition(Console.CursorLeft + 1, Console.CursorTop);
             }
-            
         }
     }
 }
